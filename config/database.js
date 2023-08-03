f9bed363910e4b6d9b6a990be666799ad7cdb577
rushi10012002 <102944910+rushi10012002@users.noreply.github.com>
2023-08-03 15:47:04 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const DatabaseConnection=()=>{
-  return  mongoose.connect("database url").then(()=>{
+  return  mongoose.connect("mongodb+srv://rushikesh:jI9GOUcjkL4lPHmX@cluster0.tgaao.mongodb.net/userList").then(()=>{
         console.log("connection success");
     }).catch(()=>{
         console.log("connection Failed");
